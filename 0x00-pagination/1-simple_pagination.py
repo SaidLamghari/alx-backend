0b903b87893d3ba0c09a1376e71b821ccc72c1f8
SaidLamghari <said.lmg@gmail.com>
2024-07-21 23:38:07 +0000

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,56 +1,82 @@
 #!/usr/bin/env python3
 """
-Implémentation de la pagination hypermédia
+Simple pagination implementation
+Auteur SAID LAMGHARI
 """
 
 import csv
-import math
-from typing import List, Dict
-from 1-simple_pagination import index_range, Server as SimpleServer
+from typing import List, Tuple
 
 
-class Server(SimpleServer):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Classe Server pour paginer une base de données de
-    noms populaires de bébés avec des métadonnées hypermédia.
+    Retourne un tuple de taille deux contenant
+    l'indice de début et l'indice de fin
+    correspondant à la plage d'indices à retourner
+    dans une liste pour les paramètres
+    de pagination donnés.
+
+    Args:
+        page (int): Numéro de page indexé à partir de 1.
+        page_size (int): Nombre d'éléments par page.
+
+    Returns:
+        Tuple[int, int]: Un tuple contenant l'indice de début (inclusif) et
+        l'indice de fin (exclusif) pour la
+        page et la taille de page spécifiées.
     """
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+    strt_indx = (page - 1) * page_size
+    end_ind = strt_indx + page_size
+    return (strt_indx, end_ind)
+
+
+class Server:
+    """Classe Server pour paginer une base de
+    données de prénoms populaires de bébé.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Dataset mis en cache
         """
-        Récupère une page du jeu de données avec des métadonnées hypermédia.
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Omettre l'en-tête
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Récupère la page spécifiée à partir du dataset paginé.
 
         Args:
-        - page (int): Numéro de la page à récupérer (par défaut: 1).
-        - page_size (int): Nombre d'éléments par page (par défaut: 10).
+            page (int): Numéro de page à récupérer (par défaut: 1).
+            page_size (int): Nombre d'éléments par page (par défaut: 10).
 
         Returns:
-        - Dict: Un dictionnaire contenant les métadonnées
-            hypermédia pour la page demandée.
-          - "page_size": Nombre d'éléments dans la page actuelle.
-          - "page": Numéro de la page actuelle.
-          - "data": Les données de la page actuelle.
-          - "next_page": Numéro de la page suivante,
-            ou None si c'est la dernière page.
-          - "prev_page": Numéro de la page précédente,
-          ou None si c'est la première page.
-          - "total_pages": Nombre total de pages dans le jeu de données.
+            List[List]: Liste des lignes correspondant à la page demandée.
         """
-        # Récupère les données de la page
-        # demandée à l'aide de la méthode héritée
-        data = self.get_page(page, page_size)
+        "La page doit être un entier positif."
+        assert isinstance(page, int) and page > 0,
+        "La taille de la page doit être un entier positif."
+        assert isinstance(page_size, int) and page_size > 0,
 
-        # Récupère l'ensemble des données
-        # pour calculer le nombre total de pages
         dataset = self.dataset()
-        total_pages = math.ceil(len(dataset) / page_size)
+        total_rows = len(dataset)
 
-        # Construit et retourne le dictionnaire
-        # de métadonnées hypermédia
-        return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if page < total_pages else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_pages": total_pages
-        }
+        # Calcul des indices de début et de fin pour la pagination
+        strt_indx, end_ind = index_range(page, page_size)
+
+        # Vérification si les indices sont hors limites
+        if strt_indx >= total_rows:
+            return []
+        if end_ind > total_rows:
+            end_ind = total_rows
+
+        return dataset[strt_indx:end_ind]
